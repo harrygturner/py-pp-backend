5426fffd2c08153c9710c172e1a75a814a05dc0d
github username <github email address>
2019-05-10 16:02:51 +0100

    all routes added and server up and running

--- src/app.py
+++ src/app.py
@@ -13,6 +13,7 @@ api = Api(app)
 # from routes.kicker import *
 # from routes.team import *
 
+# --------------------- team routes ----------------------
 @app.route('/teams', methods=['GET'])
 def get_teams():
    cur = db_connect.cursor()
@@ -52,6 +53,7 @@ def get_team_kickers(team_id):
 
    return new_format
 
+# --------------- kicker routes -----------------------
 @app.route('/kickers', methods=['GET'])
 def get_kickers():
    cur = db_connect.cursor()
@@ -64,6 +66,106 @@ def get_kickers():
    response = jsonify(new_format)
    response.status_code = 200
    return response
+
+
+# --------------- game_week routes -------------------
+@app.route('/gameweeks', methods=['GET'])
+def get_game_weeks():
+   cur = db_connect.cursor()
+   cur.execute("SELECT * FROM game_week;")
+   result = cur.fetchall()
+   new_format = []
+   for i in result:
+      matches = get_game_week_matches(i[0])
+      to_add = {'id': i[0], 'week_number': i[1], 'matches': matches}
+      new_format.append(to_add)
+
+   response = jsonify(new_format)
+   response.status_code = 200
+   return response
+
+@app.route('/gameweek/<week_number>', methods=['GET'])
+def get_week_matches(week_number):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT * FROM match WHERE game_week_id = " + str(get_game_week_id(week_number)) )
+   result = cur.fetchall()
+   new_format = []
+   for i in result:
+      to_add = { 'id': i[0], 'home_team': get_team_name(i[1]), 'away_team': get_team_name(i[2]), 'home_score': i[3], 'away_score': i[4] }
+      new_format.append(to_add)
+
+   response = jsonify(new_format)
+   response.status_code = 200
+   return response
+
+def get_game_week_matches(id):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT home_team_id, away_team_id, home_score, away_score FROM match WHERE game_week_id = " + str(id) )
+   result = cur.fetchall()
+   new_format = []
+   for i in result:
+      to_add = { 'home_team': get_team_name(i[0]), 'away_team': get_team_name(i[1]), 'home_score': i[2], 'away_score': i[3] }
+      new_format.append(to_add)
+
+   return new_format
+
+def get_team_name(id):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT name FROM team WHERE id = " + str(id) )
+   result = cur.fetchone()
+   return result[0]
+
+def get_game_week_id(week_number):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT id FROM game_week WHERE number = " + str(week_number) )
+   result = cur.fetchone()
+   return result[0]
+
+# ------------------ match routes -------------------
+@app.route('/matches', methods=['GET'])
+def get_matches():
+   cur = db_connect.cursor()
+   cur.execute( "SELECT * FROM match;" ) 
+   result = cur.fetchall() 
+   new_format = []
+   for i in result:
+      team_kickers = get_team_kickers(i[0])
+      to_add = {'id': i[0], 'home_team': get_team_name(i[1]), 'away_team': get_team_name(i[2]), 'home_score': i[3], 'away_score': i[4]}
+      new_format.append(to_add)
+
+   response = jsonify(new_format)
+   response.status_code = 200
+   return response
+
+@app.route('/matches/<id>', methods=['GET'])
+def get_match(id):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT * FROM match WHERE id = " + str(id) ) 
+   result = cur.fetchone() 
+   match_kicks = get_match_kicks(id)
+   new_format = {'id': result[0], 'home_team': get_team_name(result[1]), 'away_team': get_team_name(result[2]), 'home_score': result[3], 'away_score': result[4], 'kicks': match_kicks}
+
+   response = jsonify(new_format)
+   response.status_code = 200
+   return response
+
+def get_match_kicks(match_id):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT kicker_id, successful, distance, angle, pressure_score FROM kick WHERE match_id = " + str(match_id) )
+   result = cur.fetchall()
+   new_format = []
+   for i in result:
+      to_add = { 'kicker': get_kicker_name(i[0]), 'successful': i[1], 'distance': i[2], 'angle': i[3], 'pressure_score': i[4] }
+      new_format.append(to_add)
+
+   return new_format
+
+def get_kicker_name(id):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT name FROM kicker WHERE id = " + str(id) )
+   result = cur.fetchone()
+   return result[0]
+
 
 if __name__ == '__main__':
    app.run(port='3000')
