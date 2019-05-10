5c73701eb1c18beca1733421210c9ad391142671
github username <github email address>
2019-05-10 15:05:33 +0100

    serializer done for teams

--- src/app.py
+++ src/app.py
@@ -16,13 +16,41 @@ api = Api(app)
 @app.route('/teams', methods=['GET'])
 def get_teams():
    cur = db_connect.cursor()
-   cur.execute("SELECT id, name FROM team;") 
+   cur.execute( "SELECT id, name FROM team;" ) 
    result = cur.fetchall() 
    new_format = []
    for i in result:
-      to_add = {'id': i[0], 'name': i[1]}
+      team_kickers = get_team_kickers(i[0])
+      to_add = {'id': i[0], 'name': i[1], 'kickers': team_kickers}
       new_format.append(to_add)
-   return jsonify({ 'Teams': new_format })
+
+   response = jsonify(new_format)
+   response.status_code = 200
+   return response
+
+@app.route('/teams/<id>', methods=['GET'])
+def get_team(id):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT * FROM team WHERE id = " + str(id) )
+   result = cur.fetchall() 
+   team_kickers = get_team_kickers(id)
+   for i in result:
+      new_format = { 'id': i[0], 'name': i[1], 'league_id': i[2], 'kickers': team_kickers }
+
+   response = jsonify(new_format)
+   response.status_code = 200
+   return response
+
+def get_team_kickers(team_id):
+   cur = db_connect.cursor()
+   cur.execute( "SELECT name, position FROM kicker WHERE team_id = " + str(team_id) )
+   result = cur.fetchall()
+   new_format = []
+   for i in result:
+      to_add = { 'name': i[0], 'position': i[1] }
+      new_format.append(to_add)
+
+   return new_format
 
 @app.route('/kickers', methods=['GET'])
 def get_kickers():
@@ -33,7 +61,9 @@ def get_kickers():
    for i in result:
       to_add = {'id': i[0], 'name': i[1], 'position': i[2], 'team_id': i[3]}
       new_format.append(to_add)
-   return jsonify({ 'Kickers': new_format })
+   response = jsonify(new_format)
+   response.status_code = 200
+   return response
 
 if __name__ == '__main__':
    app.run(port='3000')
